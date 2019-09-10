ccf37476f54753c76cc97c6f264871afa5672670
TracyTN <tracytn0411@gmail.com>
2019-09-10 01:29:10 -0700

    Update modal layout for portfolio.

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,12 +1,14 @@
 var projectsArray = [
 {
   projectName: "South Park",
-  thumbnail: "assets/images/SP.jpg",
+  thumbnail: "assets/images/SP.png",
   modalID: "southPark",
   modalLabel: "unit3b",
   modalTitle: "Word Guess Game",
+  modalImg: "assets/images/SP-modal.png",
   gitRepo: "https://github.com/tracytn0411/Word-Guess-Game",
-  gitPage: "https://tracytn0411.github.io/Word-Guess-Game/"
+  gitPage: "https://tracytn0411.github.io/Word-Guess-Game/",
+  description: "A computer game that runs in the browser, and feature dynamically updated HTML and CSS powered by  JavaScript code.  The theme of the game is South Park, where user will guess a song that has been play in this comedy TV show."
 },
 {
   projectName: "Health Trivia",
@@ -14,8 +16,10 @@ var projectsArray = [
   modalID: "triviaOne",
   modalLabel: "unit5a",
   modalTitle: "General Health Trivia",
+  modalImg: "assets/images/Trivia1-modal.png",
   gitRepo: "https://github.com/tracytn0411/Trivia-Game",
   gitPage: "https://tracytn0411.github.io/Trivia-Game/",
+  description: "A trivia quiz with multiple choice options. The player has a limited amount of time to finish the whole quiz."
 },
 {
   projectName: "Silicon Valley",
@@ -23,17 +27,21 @@ var projectsArray = [
   modalID: "triviaTwo",
   modalLabel: "unit5b",
   modalTitle: "Silicon Valley Trivia",
+  modalImg: "assets/images/Trivia2-modal.png",
   gitRepo: "https://github.com/tracytn0411/Trivia-Game-2",
   gitPage: "https://tracytn0411.github.io/Trivia-Game-2/",
+  description: "A trivia game that shows only one question until player answers it or their time runs out."
 },
 {
   projectName: "Star Wars",
   thumbnail: "assets/images/StarWars.png",
   modalID: "starWars",
   modalLabel: "unit4b",
-  modalTitle: "Role Playing Game",
+  modalTitle: "The Last Jedi",
+  modalImg: "assets/images/StarWars-modal.png",
   gitRepo: "https://github.com/tracytn0411/RPG-Game",
   gitPage: "https://tracytn0411.github.io/RPG-Game/",
+  description: "A role playing game."
 },
 {
   projectName: "Crystal Collector",
@@ -85,12 +93,15 @@ var projectsArray = [
 
 function displayGallery() {
   $.each(projectsArray, function () {
-    var divCard = $("<div>").addClass("card");
-    var cardBtn = $("<button>").addClass("btn btn-light").attr({
+    var divCard = $("<div>").addClass("card bg-light");
+    var cardBtn = $("<button>").addClass("btn m-1").attr({
       "data-toggle" : "modal",
       "data-target" : "#" + this.modalID,
     });
-    var btnImg = $("<img>").addClass("img-fluid").attr("src", this.thumbnail);
+    var btnImg = $("<img>").addClass("img-fluid").attr({
+      "src": this.thumbnail,
+      "alt": this.projectName
+    });
     var cardModal = $("<div>").addClass("modal fade").attr({
       "id" : this.modalID,
       "tabindex" : "-1",
@@ -106,13 +117,23 @@ function displayGallery() {
         "aria-label" : "Close"
       }).append("<i class='fas fa-times fa-xs'></i>")
     ])
-    var modalBody = $("<div>").addClass("modal-body").text("textiete");
+
+    var modalBody = $("<div>").addClass("modal-body").append([
+      $("<div>").addClass("container-fluid")
+    ]);
+    var projectImg = $("<img>").addClass("img-fluid").attr("src", this.modalImg);
+    var projectDescription = $("<em>").text(this.description);
+
+    var modalRow = $("<div>").addClass("row").append([
+      $("<div>").addClass("col-md-7").append(projectImg),
+      $("<div>").addClass("col-md-5").append(projectDescription)
+    ])
 
     var modalFooter = $("<div>").addClass("modal-footer");
     var btnGit = $("<a>").addClass("btn btn-secondary").attr("href", this.gitRepo).append("<i class='fab fa-github'></i>");
     var btnDemo = $("<a>").addClass("btn btn-primary").attr("href", this.gitPage).append("<i class='fas fa-eye'></i>");
 
-    var modalDialog = $("<div>").addClass("modal-dialog").attr("role", "document");
+    var modalDialog = $("<div>").addClass("modal-dialog modal-lg").attr("role", "document");
     var modalContent = $("<div>").addClass("modal-content");
 
     divCard.append(cardBtn);
@@ -123,6 +144,7 @@ function displayGallery() {
     modalDialog.append(modalContent);
     modalContent.append(modalHeader);
     modalContent.append(modalBody);
+    modalBody.append(modalRow);
     modalContent.append(modalFooter);
     modalFooter.append(btnGit);
     modalFooter.append(btnDemo);
